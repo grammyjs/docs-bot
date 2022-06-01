ad8437f6e5434d78b14d761e57077fdaa89bb8a3
Roj <ez@roj.im>
2022-06-01 20:28:09 +0300

    Recreate the `Hit` interface

--- bot.ts
+++ bot.ts
@@ -1,4 +1,3 @@
-// deno-lint-ignore-file no-explicit-any camelcase
 import {
   Bot,
   InlineKeyboard,
@@ -45,6 +44,12 @@ async function search(query: string) {
   const res = await fetch(SEARCH_URL, { method: "POST", headers, body });
   return await res.json();
 }
+interface Hit {
+  objectID: string;
+  content: string;
+  url: string;
+  hierarchy: Record<`lvl${1 | 2 | 3 | 4 | 5 | 6}`, string>;
+}
 
 const replacementExp = /\+([^+]+(?:\|[^+]|))\+/g;
 
@@ -66,7 +71,7 @@ bot.on("inline_query", async (ctx) => {
   const { hits } = await search(whatToSearch(query));
   hits.length = Math.min(50, hits.length);
   await ctx.answerInlineQuery(
-    results.concat(hits.map((h: any): InlineQueryResultArticle => {
+    results.concat(hits.map((h: Hit): InlineQueryResultArticle => {
       const { title, iv, url } = getText(h, !h.hierarchy.lvl2);
       const message_text = `${title}${ZWSP}\n\n${url}`;
       const entities: MessageEntity[] = [
@@ -94,13 +99,13 @@ if (Deno.env.get("DEBUG")) {
   serve(webhookCallback(bot, "std/http"));
 }
 
-function getTitle(hit: any) {
+function getTitle(hit: Hit) {
   const h = hit.hierarchy;
   const headers = [h.lvl1, h.lvl2, h.lvl3, h.lvl4, h.lvl5, h.lvl6];
   return headers.filter((t) => !!t).join(" / ");
 }
 
-function getText(hit: any, strip: boolean) {
+function getText(hit: Hit, strip: boolean) {
   const title = getTitle(hit);
   const url = strip ? stripAnchor(hit.url) : hit.url;
   const iv = `https://t.me/iv?rhash=ca1d23e111bcad&url=${url}`;
@@ -129,7 +134,7 @@ async function makeReplacements(
     searchQueries.push(s[0]);
     return s;
   });
-  const hits = new Array<any>();
+  const hits = new Array<Hit>();
   for (const query of searchQueries) {
     hits.push((await search(query)).hits[0]);
   }
