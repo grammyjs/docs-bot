88b4847639326a674f34952890ea19fb8c5ef107
KnorpelSenf <shtrog@gmail.com>
2023-01-28 17:46:15 +0100

    feat: rewrite

--- bot.ts
+++ bot.ts
@@ -1,95 +1,72 @@
 import {
   Bot,
   InlineKeyboard,
+  type InlineQueryResultArticle,
+  serve,
   webhookCallback,
-} from "https://deno.land/x/grammy@v1.8.3/mod.ts";
-import { serve } from "https://deno.land/std@0.141.0/http/server.ts";
+} from "./deps.ts";
 import {
-  InlineQueryResultArticle,
-  MessageEntity,
-} from "https://deno.land/x/grammy@v1.8.3/platform.deno.ts";
-
-const ZWSP = "\u200b"; // zero-width space character
-
-const APPLICATION_ID = "RBF5Q0D7QV";
-const API_KEY = "33782ffb584887e3b8cdf9e760ea8e60";
-
-const SEARCH_HOST = `https://${APPLICATION_ID}-dsn.algolia.net`;
-const SEARCH_INDEX = "grammy";
-const SEARCH_URL = `${SEARCH_HOST}/1/indexes/${SEARCH_INDEX}/query`;
+  getTitle,
+  render,
+  renderInputMessageContent,
+  renderNext,
+  renderSingle,
+} from "./render.ts";
+import { parse } from "./parse.ts";
+import { search, searchOne } from "./search.ts";
 
 const token = Deno.env.get("BOT_TOKEN");
 if (token === undefined) throw new Error("Missing BOT_TOKEN");
 
 const bot = new Bot(token);
 
-bot.drop((ctx) => ctx.msg?.via_bot?.id === ctx.me.id).on(
-  "message",
-  (ctx) =>
-    ctx.reply("I can search for grammY documentation inline.", {
+bot.drop((ctx) => ctx.msg?.via_bot?.id === ctx.me.id)
+  .on("message", async (ctx) => {
+    await ctx.reply("I can search for grammY documentation inline.", {
       reply_markup: new InlineKeyboard()
         .switchInlineCurrent("Search here").row()
         .switchInline("Share article"),
-    }),
-);
-
-const enc = new TextEncoder();
-const headers = {
-  "X-Algolia-API-Key": API_KEY,
-  "X-Algolia-Application-Id": APPLICATION_ID,
-};
-async function search(query: string) {
-  const params = new URLSearchParams({ query, facetFilters: '["lang:en-US"]' });
-  const body = enc.encode(JSON.stringify({ params: params.toString() }));
-  const res = await fetch(SEARCH_URL, { method: "POST", headers, body });
-  return await res.json();
-}
-interface Hit {
-  objectID: string;
-  content: string;
-  url: string;
-  hierarchy: Record<`lvl${1 | 2 | 3 | 4 | 5 | 6}`, string>;
-}
-
-const replacementExp = /\+([^+]+(?:\|[^+]|))\+/g;
+    });
+  });
 
 bot.on("inline_query", async (ctx) => {
-  const query = ctx.inlineQuery.query;
-  const results = new Array<InlineQueryResultArticle>();
-  if (query.match(replacementExp)) {
-    const [message_text, entities, description] = await makeReplacements(query);
-    results.push(
-      {
-        id: crypto.randomUUID(),
-        type: "article",
-        title: "Make replacements",
-        description,
-        input_message_content: { message_text, entities },
-      },
-    );
-  }
-  const { hits } = await search(whatToSearch(query));
-  hits.length = Math.min(50, hits.length);
-  await ctx.answerInlineQuery(
-    results.concat(hits.map((h: Hit): InlineQueryResultArticle => {
-      const { title, iv, url } = getText(h, !h.hierarchy.lvl2);
-      const message_text = `${title}${ZWSP}\n\n${url}`;
-      const entities: MessageEntity[] = [
-        { type: "bold", offset: 0, length: title.length },
-        { type: "text_link", offset: title.length, length: 1, url: iv },
-      ];
-      return {
-        id: h.objectID,
-        type: "article",
-        title,
-        description: `${title}: ${
-          h.content ?? "Title matches the search query"
-        }`,
-        input_message_content: { message_text, entities },
-      };
+  const { query, offset } = ctx.inlineQuery;
+  const { currentQuery, texts, completedQueries } = parse(query);
+  const completed = await Promise.all(
+    completedQueries.map(async (query) => ({
+      query,
+      hit: await searchOne(query.query),
     })),
-    { cache_time: 24 * 60 * 60 }, // 24 hours (algolia re-indexing)
   );
+  let nextOffset = offset;
+  const links = completed.map(({ query, hit }) => ({
+    text: query.label ?? getTitle(hit),
+    url: hit.url,
+  }));
+  let results: InlineQueryResultArticle[];
+  if (currentQuery === undefined) {
+    // only completed queries, render them
+    results = [render(texts, links)];
+  } else {
+    // pending current query
+    const off = parseInt(offset, 10);
+    const hits = isNaN(off)
+      ? await search(currentQuery.query)
+      : await search(currentQuery.query, { offset: off });
+    nextOffset += hits.length;
+    if (texts.length === 0) {
+      // no rendering
+      results = hits.map(renderSingle);
+    } else {
+      // render and continue to search
+      const content = renderInputMessageContent(texts, links);
+      results = hits.map((hit) => renderNext(content, hit, currentQuery.label));
+    }
+  }
+  await ctx.answerInlineQuery(results, {
+    next_offset: nextOffset,
+    cache_time: 24 * 60 * 60, // 24 hours (algolia re-indexing)
+  });
 });
 
 if (Deno.env.get("DEBUG")) {
@@ -99,71 +76,53 @@ if (Deno.env.get("DEBUG")) {
   serve(webhookCallback(bot, "std/http"));
 }
 
-function getTitle(hit: Hit) {
-  const h = hit.hierarchy;
-  const headers = [h.lvl1, h.lvl2, h.lvl3, h.lvl4, h.lvl5, h.lvl6];
-  return headers.filter((t) => !!t).join(" / ");
-}
-
-function getText(hit: Hit, strip: boolean) {
-  const title = getTitle(hit);
-  const url = strip ? stripAnchor(hit.url) : hit.url;
-  const iv = `https://t.me/iv?rhash=ca1d23e111bcad&url=${url}`;
-  return { title, iv, url };
-}
-
-function stripAnchor(url: string) {
-  const index = url.lastIndexOf("#");
-  return index > 0 ? url.substring(0, index) : url;
-}
-
-function whatToSearch(query: string): string {
-  const match = query.match(/\+([^\+]+)$/);
-  if (match) {
-    return match[1];
-  }
-  return query;
-}
-async function makeReplacements(
-  text: string,
-): Promise<[string, MessageEntity[], string]> {
-  const searchQueries = new Array<string>();
-  text = `${ZWSP}${text}`;
-  text.replace(replacementExp, (_, s, o) => {
-    s = s.split("|");
-    searchQueries.push(s[0]);
-    return s;
-  });
-  const hits = new Array<Hit>();
-  for (const query of searchQueries) {
-    hits.push((await search(query)).hits[0]);
-  }
-  let matches = 0;
-  let lengthChange = 0;
-  const pathnames = new Array<string>();
-  const entities = new Array<MessageEntity>();
-  return [
-    text.replace(replacementExp, (_, s, o) => {
-      const hit = hits[matches];
-      const { title, url, iv } = getText(hit, !hit.hierarchy.lvl2);
-      if (matches == 0) {
-        entities.push({ offset: 0, length: 1, type: "text_link", url: iv });
-      }
-      const pathname = new URL(url).pathname;
-      pathnames.push(pathname);
-      const untouchedS = s;
-      s = s.split("|")[1] || title;
-      entities.push({
-        offset: (matches == 0 ? o : o - (matches * 2)) + lengthChange,
-        length: s.length,
-        type: "text_link",
-        url,
-      });
-      matches++;
-      lengthChange += s.length - untouchedS.length;
-      return s;
-    }),
-    entities,
-    pathnames.join(", "),
-  ];
-}
+// function whatToSearch(query: string): string {
+//   const match = query.match(/\+([^\+]+)$/);
+//   if (match) {
+//     return match[1];
+//   }
+//   return query;
+// }
+// async function makeReplacements(
+//   text: string,
+// ): Promise<[string, MessageEntity[], string]> {
+//   const searchQueries = new Array<string>();
+//   text = `${ZWSP}${text}`;
+//   text.replace(replacementExp, (_, s, o) => {
+//     s = s.split("|");
+//     searchQueries.push(s[0]);
+//     return s;
+//   });
+//   const hits = new Array<Hit>();
+//   for (const query of searchQueries) {
+//     hits.push((await search(query)).hits[0]);
+//   }
+//   let matches = 0;
+//   let lengthChange = 0;
+//   const pathnames = new Array<string>();
+//   const entities = new Array<MessageEntity>();
+//   return [
+//     text.replace(replacementExp, (_, s, o) => {
+//       const hit = hits[matches];
+//       const { title, url, iv } = getText(hit, !hit.hierarchy.lvl2);
+//       if (matches == 0) {
+//         entities.push({ offset: 0, length: 1, type: "text_link", url: iv });
+//       }
+//       const pathname = new URL(url).pathname;
+//       pathnames.push(pathname);
+//       const untouchedS = s;
+//       s = s.split("|")[1] || title;
+//       entities.push({
+//         offset: (matches == 0 ? o : o - (matches * 2)) + lengthChange,
+//         length: s.length,
+//         type: "text_link",
+//         url,
+//       });
+//       matches++;
+//       lengthChange += s.length - untouchedS.length;
+//       return s;
+//     }),
+//     entities,
+//     pathnames.join(", "),
+//   ];
+// }
---
+++ render.ts
@@ -0,0 +1,110 @@
+import {
+  type InlineQueryResultArticle,
+  type InputTextMessageContent,
+  type MessageEntity,
+} from "./deps.ts";
+import { type Hit } from "./search.ts";
+
+const ZWSP = "\u200b"; // zero-width space character
+
+export interface Link {
+  text: string;
+  url: string;
+}
+
+export function renderSingle(h: Hit): InlineQueryResultArticle {
+  const link = getLink(h);
+  const title = link.text;
+  const message = renderSingleInputMessageContent(link);
+  return {
+    id: h.objectID,
+    type: "article",
+    title,
+    description: `${title}: ${h.content ?? "Title matches the search query"}`,
+    input_message_content: message,
+  };
+}
+function renderSingleInputMessageContent({ text, url }: Link) {
+  const message_text = `${text}${ZWSP}\n\n${url}`;
+  const entities: MessageEntity[] = [
+    { type: "bold", offset: 0, length: text.length },
+    { type: "text_link", offset: text.length, length: 1, url: toIV(url) },
+  ];
+  return { message_text, entities };
+}
+
+export function render(
+  texts: string[],
+  links: Link[],
+): InlineQueryResultArticle {
+  return {
+    id: crypto.randomUUID(),
+    type: "article",
+    title: `Share ${links.length === 1 ? "link" : `${links.length} links`}`,
+    input_message_content: renderInputMessageContent(texts, links),
+  };
+}
+export function renderInputMessageContent(
+  texts: string[],
+  links: Link[],
+): InputTextMessageContent {
+  let message = "";
+  const entities: MessageEntity[] = [];
+  for (let i = 0; i < texts.length; i++) {
+    message += texts[i];
+    if (i < links.length) {
+      let offset = message.length;
+      const { text, url } = links[i];
+      if (i === 0) {
+        message += ZWSP;
+        offset = message.length;
+        entities.push({ type: "text_link", offset, length: 1, url: toIV(url) });
+      }
+      message += text;
+      entities.push({ type: "text_link", offset, length: text.length, url });
+    }
+  }
+  return { message_text: message, entities };
+}
+
+export function renderNext(
+  existing: InputTextMessageContent,
+  hit: Hit,
+  label?: string,
+): InlineQueryResultArticle {
+  const { message_text, entities = [] } = existing;
+  const { text, url } = getLink(hit);
+  const labelOrText = label ?? text;
+  return {
+    id: crypto.randomUUID(),
+    type: "article",
+    title: text,
+    input_message_content: {
+      message_text: message_text + labelOrText,
+      entities: [...entities, {
+        type: "text_link",
+        offset: message_text.length,
+        length: labelOrText.length,
+        url,
+      }],
+    },
+  };
+}
+
+function getLink(hit: Hit, strip = !hit.hierarchy.lvl2): Link {
+  const text = getTitle(hit);
+  const url = strip ? stripAnchor(hit.url) : hit.url;
+  return { text, url };
+}
+export function getTitle(hit: Hit) {
+  const h = hit.hierarchy;
+  const headers = [h.lvl1, h.lvl2, h.lvl3, h.lvl4, h.lvl5, h.lvl6];
+  return headers.filter((t) => !!t).join(" / ");
+}
+function stripAnchor(url: string) {
+  const index = url.lastIndexOf("#");
+  return index > 0 ? url.substring(0, index) : url;
+}
+function toIV(url: string) {
+  return `https://t.me/iv?rhash=ca1d23e111bcad&url=${url}`;
+}
